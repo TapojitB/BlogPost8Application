af233cff03e705a6325819e4167f252a275a6d91
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-11-14 12:51:12 +0530

    Zuul...

--- common-gateway-server/src/main/java/com/evergreenprogrammer/microservices/commongatewayserver/CommonGatewayServerApplication.java
+++ common-gateway-server/src/main/java/com/evergreenprogrammer/microservices/commongatewayserver/CommonGatewayServerApplication.java
@@ -4,23 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableDiscoveryClient(autoRegister=true)
+@EnableDiscoveryClient(autoRegister = true)
 @EnableZuulProxy
 public class CommonGatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonGatewayServerApplication.class, args);
-	}
-	
-	@Bean
-	public Sampler defaultSampler() {
-		return Sampler.ALWAYS_SAMPLE;
-
 	}
 
 }
